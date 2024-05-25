cbed99946233da40d6a0ae26aa1078923698d30e
Andreas Tsouris <andtsouris@gmail.com>
2024-05-25 18:50:36 +0200

    Some comments and added a gitignore file.

--- pom_timer.sh
+++ pom_timer.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# Welcome text
 echo "Welcome to PomodoroCLI!"
 echo "Let's automatically launch a $1 second interval."
 
 
-
+# Basic timer function
+# arg1: timer duration (seconds)
+# arg2: progress bar length
 launch_timer () {
   counter=0
   divider=20
@@ -18,5 +22,7 @@ launch_timer () {
   echo ""
   echo "All done!"
 }
+
+# Main script
 cur_duration=$1
 launch_timer $cur_duration
