41a15f20ec4b6b94c6d9e84780339726b66057eb
Andreas Tsouris <andtsouris@gmail.com>
2024-05-28 20:21:33 +0200

    Implemented pomodoro sequence

--- functions.sh
+++ functions.sh
@@ -20,8 +20,12 @@ print_pbar(){
   # Calculating the pbar symbols
   (( _hash_num=$current ))
   (( _dash_num=bar_len-_hash_num))
-  hash_string=$(seq -s# ${_hash_num}|tr -d '[:digit:]')
-  title_block=$(echo " $title")
+  if [ $_hash_num -eq 0 ]; then
+    hash_string=''
+  else
+    hash_string=$(seq -s# $_hash_num |tr -d '[:digit:]') 
+  fi
+   title_block=$(echo " $title")
 
 # Printing with or without the message depending on the window size
   if [ $_blank_block -lt 2 ]
@@ -38,23 +42,73 @@ launch_timer (){
   local msg="$2"
   local bar_len="$3"
   local duration="$4"
-  echo "BAR_LEN:${bar_len};   DURATION:${duration}"
+  #echo "BAR_LEN:${bar_len};   DURATION:${duration}"
   counter=0
+  local partial=0
+  rem_secs=$duration
   fraction=$(echo "scale=2; $duration/ $bar_len" | bc) 
   while [ $counter -ne $bar_len ]
-    do
+  do
+    local partial=$(($partial+1))
+    if (( $(echo "$partial > $fraction" | bc -l) ))
+    then
       local counter=$(($counter+1))
+      local partial=0
+    fi
+    #(( cur_perc=(counter*100)/bar_len ))
+    #(( rem_secs=duration-(duration*cur_perc/100) ))
+    (( rem_secs-=1))
+    (( m=rem_secs/60 ))
+    (( s=rem_secs%60 ))
+    local time=$(printf "%02d:%02d" $m $s)
 
-      (( cur_perc=(counter*100)/bar_len ))
-      (( rem_secs=duration-(duration*cur_perc/100) ))
-      (( m=rem_secs/60 ))
-      (( s=rem_secs%60 ))
-      local time=$(printf "%02d:%02d" $m $s)
+    print_pbar ${title} ${msg} ${counter} ${time} ${bar_len}
+    sleep 1
+  done
+  echo -e "\nAll done!"
+} 
+#launch_timer "My_title" "Let's_go!" 35 60
 
-      print_pbar ${title} ${msg} ${counter} ${time} ${bar_len}
-      fraction=$(echo "scale=2; $duration/ $bar_len" | bc)
-      sleep $fraction
-    done
-  echo ""
-  echo "All done!"
-}  
+launch_timer2 (){
+  local title="$1"
+  local msg="$2"
+  local bar_len="$3"
+  local duration="$4"
+
+  secs_passed=0
+
+  while [ $secs_passed -le $duration ]
+  do
+    cur_perc=$(echo "scale=2; $secs_passed/$duration" | bc)
+    counter=$( echo "scale=0; $cur_perc*$bar_len" | bc)
+
+    (( rem_secs=$duration-$secs_passed))
+    (( m=rem_secs/60 ))
+    (( s=rem_secs%60 ))
+    local time=$(printf "%02d:%02d" $m $s)
+    
+    if (( $(echo "$counter < 1" | bc -l) )); then
+      counter=0
+    fi
+    print_pbar ${title} ${msg} ${counter%.*} ${time} ${bar_len} 
+    sleep 1
+    (( secs_passed=secs_passed+1 ))
+  done
+  }
+
+start_timer() {
+  clear
+  echo -n "Timer duration in seconds: "
+  read cur_duration
+  launch_timer2 "Timer" "Timer running." 35 $cur_duration
+  afplay assets/bell.wav
+  sleep 1
+  clear
+  menu
+}
+
+repeat(){
+  cols=$(tput cols)
+  echo "Cols: $cols"
+  for i in {1..$(cols)}; do echo -n "$1"; done
+}
--- pom_timer.sh
+++ pom_timer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source ./functions.sh
-
+cols=$(tput cols)
 # Welcome text
 
 # Launching the main menu
@@ -23,44 +23,77 @@ ColorRed(){
   echo -ne $red$1$clear
 }
 
+
 start_session() {
-  clear
-  echo "Let's start our session..."
-  echo -e "
-${green}1)$clear Start a work interval
-${green}2)$clear Exit
-  "
-  my_msg="Have fun."
-  rest_msg="Let\'s get some rest."
+  local work_int=25; local s_break=5; local l_break=15, local reps=4
+# Showing a menu
+  echo -e "The default time intervals are:
+1. Work: 25 minutes 
+2. Short break: 5 minutes
+3. Long break: 15 minutes
+4. Long break after 4 work intervals
+Do you want to change these intervals [y/N]?"
   read a
-  case $a in
-    1) {
-      launch_timer "Work" "\${my_msg}" 35 2400
-      launch_timer "Break" "\{rest_msg}" 35 300
-      launch_timer "Work" "\${my_msg}" 35 2400
-      launch_timer "Break" "\{rest_msg}" 35 300
-      launch_timer "Work" "\${my_msg}" 35 2400
-      launch_timer "Break" "\{rest_msg}" 35 300 
-      launch_timer "Work" "\${my_msg}" 35 2400
-      launch_timer "Long Break" "\{rest_msg}" 35 900
-    };;
-    *) exit;;
-  esac
+  
+# Defining custom interval durations
+if [[ "$a" == "y" ]]; then
+  for ((i=1; i<=cols; i++)); do printf '=%.0s'; done
+  echo "Insert new interval durations:
+Work (minutes): "
+  read work_int
+  echo "Short break (mins): "
+  read s_break
+  echo "Long break (mins): "
+  read l_break
+  echo "Work repetition before a long break: "
+  read reps
+  fi
+
+  # Starting the session loop
+  local work=1
+  local work_done=0
+  local run=1
+  cols=$(tput cols)
+  echo ""
+  clear
+#  for ((i=1; i<=cols; i++)); do printf '=%.0s'; done
+
+  echo "Use Ctrl+c to exit."
+  while [ $run -eq 1 ]; do
+    if [[ $work -eq 1 ]]; then
+      work=0
+      launch_timer2 "Work_session" "No_slacking_allowed!" 35 $(( work_int*60 ))
+      (( work_done=work_done+1))
+    fi
+    (( to_l_break=work_done%reps))
+    if [[ $work -eq 0 ]]; then
+      work=1
+      if [[ $to_l_break -eq 0 ]]; then
+        launch_timer2 "Long_break" "Go_get_some_coffee,_you_earned_it" 10 $(( l_break*60 ))
+      else
+        launch_timer2 "Short_break" "Stretch_out_and_back_to_work." 20 $(( s_break*60 ))
+      fi 
+    fi
+  done
+
+
+
 }
 
 # Defining the main menu
 menu(){
-  echo -ne "
-$(ColorGreen '1)') Start a Pomorodo session
+  echo -ne "$(ColorGreen '1)') Start a Pomorodo session
 $(ColorGreen '2)') Start a timer
 $(ColorGreen '3)') View today's stats 
 $(ColorGreen '4)') Settings
 $(ColorGreen '0)') Quit
-"
+Select one of the options above: "
   read a
+  for ((i=1; i<=cols; i++)); do printf '=%.0s'; done
+
   case $a in
     1) start_session ;;
-    2) echo 'Timer duration (HH:MM:SS):';;
+    2) start_timer ;;
     3) echo 'Viewing stats!';;
     4) echo 'Settings opened';;
     0) exit;;
